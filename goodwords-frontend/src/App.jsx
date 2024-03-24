de334d4e245bf4955bb2cfd040d91876db47edcc
Adnanrobi <adnanrobi6@gmail.com>
2024-03-25 05:38:01 +0600

    - handled routing

---
+++ goodwords-frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegistrationPage from "./pages/RegistrationPage";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/login" />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegistrationPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
